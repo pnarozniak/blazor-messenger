5b05fb9c328f9c097cfd64743661fa69a26cabf8
xlegodroit <xxvege@gmail.com>
2021-08-10 02:46:32 +0200

    Added tables ef configuration

---
+++ Server/EfConfigurations/ConversationEfConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using messanger.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace messanger.Server.EfConfigurations
+{
+    public class ConversationEfConfiguration : IEntityTypeConfiguration<Conversation>
+    {
+        public void Configure(EntityTypeBuilder<Conversation> builder)
+        {
+            builder.ToTable("Conversation");
+
+            builder.HasKey(c => c.IdConversation)
+                .HasName("Conversation_pk");
+
+            builder.Property(c => c.IdConversation)
+                .UseIdentityColumn();
+
+            builder.Property(c => c.Name).IsRequired().HasMaxLength(64);
+
+            builder.HasMany(c => c.Messages)
+                .WithOne(m => m.IdConversationNavigation)
+                .HasForeignKey(m => m.IdConversation)
+                .HasConstraintName("Conversation_Message");
+        }
+    }
+}
---
+++ Server/EfConfigurations/FriendshipEfConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using messanger.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace messanger.Server.EfConfigurations
+{
+    public class FriendshipEfConfiguration : IEntityTypeConfiguration<Friendship>
+    {
+        public void Configure(EntityTypeBuilder<Friendship> builder)
+        {
+            builder.ToTable("Friendship");
+
+            builder.HasKey(f => new { f.IdUser1, f.IdUser2 })
+                .HasName("Friendship_pk");
+
+            builder.Property(f => f.CreatedAt).IsRequired();
+
+            builder.HasOne(f => f.IdUser1Navigation)
+                .WithMany(u1 => u1.FriendshipsWhereIsUser1)
+                .HasForeignKey(f => f.IdUser1)
+                .HasConstraintName("Friendship_User1")
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            builder.HasOne(f => f.IdUser2Navigation)
+                .WithMany(u2 => u2.FriendshipsWhereIsUser2)
+                .HasForeignKey(f => f.IdUser2)
+                .HasConstraintName("Friendship_User2")
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        }
+    }
+}
