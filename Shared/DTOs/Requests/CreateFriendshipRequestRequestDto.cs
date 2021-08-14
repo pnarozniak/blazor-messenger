0f8bdad1fce3b91e8a4b9e8f5ce2a7afa8f4a898
xlegodroit <xxvege@gmail.com>
2021-08-14 21:12:22 +0200

    Added required shared DTOs

---
+++ Shared/DTOs/Requests/CreateFriendshipRequestRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace messanger.Shared.DTOs
+{
+    public class CreateFriendshipRequestRequestDto
+    {
+        [Required]
+        [StringLength(36, MinimumLength = 36)]
+        public string IdReceiver { get; set; }
+    }
+}
---
+++ Shared/DTOs/Responses/FriendshipRequestResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace messanger.Shared.DTOs
+{
+    public class FriendshipRequestResponseDto
+    {
+        public UserResponseDto User { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
