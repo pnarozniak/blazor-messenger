b46671daaa5cd1068769391dfec26aa86f1bd8dc
xlegodroit <xxvege@gmail.com>
2021-08-14 21:12:47 +0200

    Added LoggedUserService for getting user id in controller ctor

---
+++ Server/Services/Implementations/LoggedUserService.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using messanger.Server.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace messanger.Server.Services.Implementations
+{
+    public class LoggedUserService : ILoggedUserService
+    {
+        private readonly HttpContext _httpContext;
+        public LoggedUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContext = httpContextAccessor.HttpContext;
+        }
+
+        public ClaimsPrincipal LoggedUser => _httpContext.User;
+    }
+}
