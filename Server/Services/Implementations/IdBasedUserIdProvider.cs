afb172548c5f76becba5eb2f0e0ae4b5d036299c
xlegodroit <xxvege@gmail.com>
2021-08-19 23:28:09 +0200

    Added IdBasedUserIdProvider service implementation for IdBased signalr connection recognition

---
+++ Server/Services/Implementations/IdBasedUserIdProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.SignalR;
+
+namespace messanger.Server.Services.Implementations
+{
+    public class IdBasedUserIdProvider : IUserIdProvider
+    {
+        public string GetUserId(HubConnectionContext connection)
+        {
+            return connection.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
