e70c0eedc42a6e0e875873509e6141886c2ba7b8
xlegodroit <xxvege@gmail.com>
2021-08-19 22:53:50 +0200

    Added strongly typed notifications hub

---
+++ Server/Hubs/NotificationsHub.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using messanger.Shared.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace messanger.Server.Hubs
+{
+    public interface INotificationsHub
+    {
+        public Task NewFriendshipRequest(UserResponseDto sender);
+    }
+
+    [Authorize]
+    public class NotificationsHub : Hub<INotificationsHub>
+    {
+
+    }
+}
