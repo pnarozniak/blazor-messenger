3c7d0a38c44beb7f7b80ff2497f170c44809e95e
xlegodroit <xxvege@gmail.com>
2021-08-23 19:48:08 +0200

    Added db migrations

--- Server/Data/Migrations/20210819173055_added_missing_relation_between_message_and_user.Designer.cs
+++ Server/Data/Migrations/20210819173055_added_missing_relation_between_message_and_user.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using messanger.Server.Data;
 
 namespace messanger.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210819173055_added_missing_relation_between_message_and_user")]
+    partial class added_missing_relation_between_message_and_user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -270,6 +272,7 @@ namespace messanger.Server.Data.Migrations
                         .HasColumnType("int");
 
                     b.Property<string>("Name")
+                        .IsRequired()
                         .HasMaxLength(64)
                         .HasColumnType("nvarchar(64)");
 
--- Server/Data/Migrations/20210819175356_enabled_null_assignment_to_conversation_name.Designer.cs
+++ Server/Data/Migrations/20210819175356_enabled_null_assignment_to_conversation_name.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using messanger.Server.Data;
 
 namespace messanger.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210819175356_enabled_null_assignment_to_conversation_name")]
+    partial class enabled_null_assignment_to_conversation_name
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
