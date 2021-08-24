018554aec6fad35e46288313f264616289b37891
xlegodroit <xxvege@gmail.com>
2021-08-24 03:29:29 +0200

    Added GetDataRequestDto

---
+++ Shared/DTOs/Requests/GetDataRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace messanger.Shared.DTOs.Requests
+{
+    public class GetDataRequestDto
+    {
+        [Range(0, int.MaxValue)]
+        public int Skip { get; set; }
+
+        [RegularExpression(".*\\S.*")]
+        public string Filter { get; set; }
+    }
+}
