fd9b2599da94522e33d7e577747abda850220b5f
xlegodroit <xxvege@gmail.com>
2021-08-24 03:32:25 +0200

    Added FilterHelper class responsible for filter string parsing

---
+++ Server/Helpers/FilterHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace messanger.Server.Helpers
+{
+    public static class FilterHelper
+    {
+        public static string GetFirstPart(string filter)
+        {
+            return !filter.Contains(" ") ? filter : filter.Split(" ")[0];
+        }
+
+        public static string GetLastPart(string filter)
+        {
+            return !filter.Contains(" ") ? "" : string.Join("", filter.Split(" ").Skip(1));
+        }
+    }
+}
