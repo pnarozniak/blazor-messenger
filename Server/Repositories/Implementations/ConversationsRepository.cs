c4a8924161f2abed2e1509462afdae63f5775545
xlegodroit <xxvege@gmail.com>
2021-09-01 02:48:14 +0200

    Added required methods to conversations repository

--- Server/Repositories/Implementations/ConversationsRepository.cs
+++ Server/Repositories/Implementations/ConversationsRepository.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using messanger.Server.Data;
 using messanger.Server.Helpers;
+using messanger.Server.Models;
 using messanger.Server.Repositories.Interfaces;
 using messanger.Shared.DTOs;
+using messanger.Shared.DTOs.Requests;
 using messanger.Shared.DTOs.Responses;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,12 +16,14 @@ namespace messanger.Server.Repositories.Implementations
     public class ConversationsRepository : IConversationsRepository
     {
         private readonly ApplicationDbContext _context;
+
         public ConversationsRepository(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public async Task<IEnumerable<ConversationResponseDto>> GetUserRecentConversationsAsync(string idUser, int skip)
+        public async Task<IEnumerable<ConversationResponseDto>> GetUserRecentConversationsAsync
+            (string idUser, int skip)
         {
             const int takeConversations = 10;
             const int takeConversationMembers = 5;
@@ -39,19 +44,20 @@ namespace messanger.Server.Repositories.Implementations
                         })
                         .Take(takeConversationMembers),
                     LastMessage = c.Messages
-                            .OrderByDescending(m => m.CreatedAt).Select(m => new MessageResponseDto
-                            {
-                                IdMessage = m.IdMessage,
-                                Content = m.Content,
-                                CreatedAt = m.CreatedAt
-                            }).FirstOrDefault()
+                        .OrderByDescending(m => m.CreatedAt).Select(m => new MessageResponseDto
+                        {
+                            IdMessage = m.IdMessage,
+                            Content = m.Content,
+                            CreatedAt = m.CreatedAt
+                        }).FirstOrDefault()
                 })
                 .Skip(skip)
                 .Take(takeConversations)
                 .ToListAsync();
         }
 
-        public async Task<IEnumerable<ConversationResponseDto>> GetUserConversationsMatchingFilterAsync(string idUser, string filter)
+        public async Task<IEnumerable<ConversationResponseDto>> GetUserConversationsMatchingFilterAsync
+            (string idUser, string filter)
         {
             const int takeConversations = 10;
             const int takeConversationMembers = 5;
@@ -61,7 +67,7 @@ namespace messanger.Server.Repositories.Implementations
 
             return await _context.Conversations
                 .Where(c => c.ConversationMembers.Any(cm => cm.IdUser == idUser))
-                .Where(c => (c.Name != null && c.Name.StartsWith(filter)) || c.ConversationMembers.Any(cm =>
+                .Where(c => c.Name != null && c.Name.StartsWith(filter) || c.ConversationMembers.Any(cm =>
                     cm.IdUserNavigation.FirstName.StartsWith(firstName) &&
                     cm.IdUserNavigation.LastName.StartsWith(lastName)))
                 .OrderBy(c => c.Name == null ? 2 : c.Name.StartsWith(filter) ? 0 : 1)
@@ -88,6 +94,78 @@ namespace messanger.Server.Repositories.Implementations
                         }).FirstOrDefault()
                 })
                 .Take(takeConversations)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<MessageResponseDto>> GetUserConversationMessagesAsync
+            (int idConversation, string idUser, int skip)
+        {
+            return await _context.Messages
+                .Where(m => m.IdConversation == idConversation
+                            && m.IdConversationNavigation.ConversationMembers
+                                .Any(cm => cm.IdUserNavigation.Id == idUser))
+                .OrderBy(m => m.CreatedAt)
+                .Skip(skip)
+                .Select(m => new MessageResponseDto
+                {
+                    IdMessage = m.IdMessage,
+                    Content = m.Content,
+                    CreatedAt = m.CreatedAt,
+                    Sender = new UserResponseDto
+                    {
+                        IdUser = m.IdSenderNavigation.Id,
+                        FirstName = m.IdSenderNavigation.FirstName,
+                        LastName = m.IdSenderNavigation.LastName
+                    }
+                }).ToListAsync();
+        }
+
+        public async Task<MessageResponseDto> AddNewMessageToUserConversationAsync
+            (int idConversation, string idSender, NewMessageRequestDto newMessage)
+        {
+            var conversation = await _context.Conversations
+                .SingleOrDefaultAsync
+                (c => c.IdConversation == idConversation &&
+                      c.ConversationMembers.Any(cm => cm.IdUserNavigation.Id == idSender));
+
+            if (conversation is null)
+                return null;
+
+            var message = new Message
+            {
+                Content = newMessage.Content,
+                CreatedAt = DateTime.Now,
+                IdConversationNavigation = conversation,
+                IdSender = idSender
+            };
+
+            conversation.Messages.Add(message);
+
+            if (await _context.SaveChangesAsync() <= 0)
+                return null;
+
+            return new MessageResponseDto
+            {
+                IdMessage = message.IdMessage,
+                Content = message.Content,
+                CreatedAt = message.CreatedAt,
+                Sender = await _context.Users
+                    .Where(u => u.Id == idSender)
+                    .Select(u => new UserResponseDto
+                    {
+                        IdUser = u.Id,
+                        FirstName = u.FirstName,
+                        LastName = u.LastName
+                    }).SingleOrDefaultAsync()
+            };
+        }
+
+        public async Task<IEnumerable<string>> GetConversationMembersIdsAsync
+            (int idConversation)
+        {
+            return await _context.ConversationMembers
+                .Where(cm => cm.IdConversation == idConversation)
+                .Select(cm => cm.IdUserNavigation.Id)
                 .ToListAsync();
         }
     }
