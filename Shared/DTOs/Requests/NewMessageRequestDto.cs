4f738bf3da47ac839f2519bcfe2c4b24e1e1f9ec
xlegodroit <xxvege@gmail.com>
2021-09-01 02:46:39 +0200

    Added NewMessageRequestDTO

---
+++ Shared/DTOs/Requests/NewMessageRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace messanger.Shared.DTOs.Requests
+{
+    public class NewMessageRequestDto
+    {
+        [Required]
+        public string Content { get; set; }
+    }
+}
