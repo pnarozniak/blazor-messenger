434853f4b0c9f1344ac5eff9d9e79e0c8ee1be71
xlegodroit <xxvege@gmail.com>
2021-09-08 19:35:57 +0200

    Added new DTOs

---
+++ Shared/DTOs/Requests/GetConversationsRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace messanger.Shared.DTOs.Requests
+{
+    public class GetConversationsRequestDto
+    {
+        [Range(0, int.MaxValue)] public int Skip { get; set; }
+
+        [RegularExpression(".*\\S.*")] public string Filter { get; set; }
+    }
+}
