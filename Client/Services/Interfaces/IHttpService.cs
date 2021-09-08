7e5e5fc6659fffda74d9f6b4b56961dce039ff55
xlegodroit <xxvege@gmail.com>
2021-09-09 00:03:37 +0200

    Implemented IHttpService

---
+++ Client/Services/Interfaces/IHttpService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using messanger.Client.Models;
+
+namespace messanger.Client.Services.Interfaces
+{
+    public interface IHttpService
+    {
+        Task<HttpResponseWrapper<T>> GetAsync<T>(string url, QueryParams queryParams = null);
+    }
+}
