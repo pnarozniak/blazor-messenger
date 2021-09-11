b8584465cb32fac9b96e65e787dd6249b943e2d7
xlegodroit <xxvege@gmail.com>
2021-09-11 20:33:16 +0200

    Added IAppStateService

---
+++ Client/Services/Implementations/AppStateService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using messanger.Client.Services.Interfaces;
+using messanger.Shared.DTOs.Responses;
+
+namespace messanger.Client.Services.Implementations
+{
+    public class AppStateService : IAppStateService
+    {
+        public List<ConversationResponseDto> Conversations { get; } = new();
+
+        public ConversationResponseDto ActiveConversation { get; private set; }
+
+        public event Action OnChange;
+
+        public void AddConversations(List<ConversationResponseDto> newConversations)
+        {
+            Conversations.AddRange(newConversations);
+            NotifyStateChanged();
+        }
+
+        public void SetActiveConversation(ConversationResponseDto newActiveConversation)
+        {
+            ActiveConversation = newActiveConversation;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
