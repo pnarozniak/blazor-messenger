34a97ab30bd1214f91263d8d8a4680cbd7d1dbd7
xlegodroit <xxvege@gmail.com>
2021-09-13 17:36:30 +0200

    Added SearchResults component with ResultsWrapper-model

---
+++ Client/Shared/SideBar/SearchResults.razor.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using messanger.Client.Models;
+using messanger.Client.Repositories.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace messanger.Client.Shared.SideBar
+{
+    public partial class SearchResults
+    {
+        [Parameter] public SearchResultsWrapper WrappedResults { get; set; }
+
+        [Parameter] public EventCallback StopSearchingCallback { get; set; }
+
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        [Inject] public IConversationsRepository ConversationsRepository { get; set; }
+
+        private async Task CloseResults()
+        {
+            await StopSearchingCallback.InvokeAsync();
+        }
+    }
+}
