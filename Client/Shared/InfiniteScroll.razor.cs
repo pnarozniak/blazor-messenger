42e097149938d18c205e67e2f74cc219ca29b658
xlegodroit <xxvege@gmail.com>
2021-09-21 13:36:54 +0200

    Added InfiniteScroll virtualized component

---
+++ Client/Shared/InfiniteScroll.razor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace messanger.Client.Shared
+{
+    public partial class InfiniteScroll<TItem> : IAsyncDisposable
+    {
+        private bool _hasAll;
+        private bool _loading;
+        private ElementReference _lastItemRef;
+        private IJSObjectReference _module;
+        private IJSObjectReference _instance;
+
+        [Parameter] public List<TItem> Items { get; set; }
+
+        [Parameter] public RenderFragment<TItem> ItemTemplate { get; set; }
+
+        [Parameter] public Func<ElementReference> GetScrollContainerRef { get; set; }
+
+        [Parameter] public RenderFragment LoadingTemplate { get; set; }
+
+        [Parameter] public RenderFragment HasAllTemplate { get; set; }
+
+        [Parameter] public Func<Task<bool>> LoadNextItems { get; set; }
+
+        [Parameter] public float ItemSize { get; set; } = 50f;
+
+        [Parameter] public bool ScrollReverse { get; set; } = false;
+
+        [Inject] public IJSRuntime JsRuntime { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _module = await JsRuntime.InvokeAsync<IJSObjectReference>("import", "./js/infinite-scroll.js");
+                await RunObserver();
+            }
+        }
+
+        private async Task RunObserver()
+        {
+            _instance = await _module.InvokeAsync<IJSObjectReference>("initialize", new
+            {
+                scrollContainer = GetScrollContainerRef(),
+                lastItem = _lastItemRef,
+                componentInstance = DotNetObjectReference.Create(this)
+            });
+        }
+
+        [JSInvokable]
+        public async Task OnScrollToEnd()
+        {
+            if (_loading)
+                return;
+
+            _loading = true;
+            StateHasChanged();
+
+            _hasAll = !await LoadNextItems.Invoke();
+
+            _loading = false;
+            StateHasChanged();
+        }
+
+        public async Task ForceRefresh()
+        {
+            _hasAll = false;
+            StateHasChanged();
+
+            if (_instance is not null)
+            {
+                await _instance.InvokeVoidAsync("dispose");
+                await _instance.DisposeAsync();
+            }
+
+            await RunObserver();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_module is not null)
+            {
+                await _module.DisposeAsync();
+            }
+
+            if (_instance is not null)
+            {
+                await _instance.InvokeVoidAsync("dispose");
+                await _instance.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ Client/wwwroot/js/infinite-scroll.js
@@ -0,0 +1,22 @@
+﻿export function initialize({ scrollContainer, lastItem, componentInstance }) {
+    const observerOptions = {
+        root: scrollContainer,
+        threshold: 0,
+        rootMargin: "0px"
+    };
+
+    const observer = new IntersectionObserver(async function (entries) {
+        if (entries[0].isIntersecting) {
+            await componentInstance.invokeMethodAsync("OnScrollToEnd");
+            observer.unobserve(lastItem);
+            observer.observe(lastItem);
+        }
+    },
+        observerOptions);
+
+    observer.observe(lastItem);
+
+    return {
+        dispose: () => observer.disconnect()
+    };
+}
